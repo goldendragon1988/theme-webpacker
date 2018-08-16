ba8cdff396eac6cab6ccf196643b4d52e50111b6
Jim Arnold Go <jag.arnold.go@gmail.com>
2018-08-16 21:37:00 +0800

    devise redirect sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "dashboard", to: "dashboard#index"
+  devise_for :users
+  authenticated do
+    get "/dashboard" => "dashboard#index", as: :authenticated_root
+  end
   root to: "home#index"
 end
