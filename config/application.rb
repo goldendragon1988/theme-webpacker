f3edba913872222901b79b9ebada74e5adc27860
Jim Arnold Go <jag.arnold.go@gmail.com>
2018-08-25 14:41:21 +0800

    add css for beagle

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,18 @@ module Mio
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.assets.paths << Rails.root.join("vendor", "assets", "css")
+    config.assets.paths << Rails.root.join("vendor", "assets", "lib")
+    config.assets.paths << Rails.root.join("vendor", "assets", "img")
+    config.assets.paths << Rails.root.join("vendor", "assets", "js")
+
+    config.assets.precompile += %w(
+      .svg
+      .eot
+      .woff
+      .woff2
+      .ttf
+    )
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
