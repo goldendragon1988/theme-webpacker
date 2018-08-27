ff6fac8cf6fdbde7687b596fc2bd170f99cd234b
Jim Arnold Go <jag.arnold.go@gmail.com>
2018-08-27 17:41:05 +0800

    Add app js

---
+++ app/assets/javascripts/beagle.js
@@ -0,0 +1,9 @@
+//= require jquery/jquery.min.js
+//= require perfect-scrollbar/js/perfect-scrollbar.min.js
+//= require bootstrap/dist/js/bootstrap.bundle.min.js
+//= require app.js
+//
+        $(document).ready(function(){
+          //-initialize the javascript
+          App.init();
+        });
